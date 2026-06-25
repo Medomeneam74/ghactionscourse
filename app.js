b712b244179b07358f12ec71f4999b628ea065fd
moo <medomeneam2@gmail.com>
2026-06-26 02:19:55 +0300

    fixed app

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Hello Actions!");
+const http = require('http');
+
+// بنحدد البورت اللي السيرفر هيشتغل عليه (اللي إحنا رابطينه في الدوكر)
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello from GitHub Actions! CI/CD Pipeline is working perfectly 😎\n');
+});
+
+// بنخلي السيرفر يشتغل على 0.0.0.0 عشان يقبل اتصالات من بره الكونتينر
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running and listening on port ${PORT}`);
+});
